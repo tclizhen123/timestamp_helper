7f2cd82d2685b20e8408ceef158e7cf8d723e317
lizhen21 <lizhen21@meituan.com>
2020-06-04 19:24:54 +0800

    兼容秒和毫秒的时间戳

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ sys.setdefaultencoding('utf8')
 
 
 def main(wf):
-    timestamp = int(sys.argv[1])
-    time_local = time.localtime(timestamp)
+    timestampStr = str(sys.argv[1])
+    timestamp = sys.argv[1]
+    if len(timestampStr) > 10:
+        timestamp = float(timestamp) / 1000.0
+
+    time_local = time.localtime(float(timestamp))
     dt = time.strftime("%Y-%m-%d %H:%M:%S",time_local)
     wf.add_item(title=dt,
                 subtitle=u"测试副标题",
