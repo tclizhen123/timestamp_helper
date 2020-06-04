179a1c32b67084b49e5a2f88f651e86f7e9408d0
lizhen21 <lizhen21@meituan.com>
2020-06-04 19:03:59 +0800

    时间戳可以粘贴

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 # coding: utf-8
 from workflow import Workflow, web, ICON_WEB
 import sys
+import time
 reload(sys)
 sys.setdefaultencoding('utf8')
 
 
 def main(wf):
-    wf.add_item(title=sys.argv[1],
+    timestamp = int(sys.argv[1])
+    time_local = time.localtime(timestamp)
+    dt = time.strftime("%Y-%m-%d %H:%M:%S",time_local)
+    wf.add_item(title=dt,
                 subtitle=u"测试副标题",
                 valid=True,
-                icon=ICON_WEB)
+                icon=ICON_WEB,
+                arg=dt)
 
     wf.send_feedback()
 
