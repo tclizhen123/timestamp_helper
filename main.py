e09b0acc0ca2f2644c4d192a8c55b7adcbb73b94
lizhen21 <lizhen21@meituan.com>
2020-06-05 12:56:50 +0800

    支持从剪切板读取时间戳

--- main.py
+++ main.py
@@ -2,20 +2,27 @@
 from workflow import Workflow, web, ICON_WEB
 import sys
 import time
+import pyperclip
 reload(sys)
 sys.setdefaultencoding('utf8')
 
 
 def main(wf):
-    timestampStr = str(sys.argv[1])
-    timestamp = sys.argv[1]
+    if len(sys.argv) == 1:
+        timestampStr = str(pyperclip.paste())
+        timestamp = float(pyperclip.paste())
+    else:
+        timestamp = sys.argv[1]
+        timestampStr = str(sys.argv[1])
+
+    
     if len(timestampStr) > 10:
         timestamp = float(timestamp) / 1000.0
 
     time_local = time.localtime(float(timestamp))
     dt = time.strftime("%Y-%m-%d %H:%M:%S",time_local)
     wf.add_item(title=dt,
-                subtitle=u"测试副标题",
+                subtitle=timestampStr,
                 valid=True,
                 icon=ICON_WEB,
                 arg=dt)
